1c1de2b31555e9c2d1c62ee5a0e830c75c5c3e86
Jacob Cowan <mrjcowman@gmail.com>
2020-11-02 21:36:24 -0800

    Set up basic api routing

--- server.js
+++ server.js
@@ -16,27 +16,57 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/exercisedb", {u
 
 // Seed here?
 
-// ROUTES ==================================
-app.get("/", (req, res)=>{
-    // TODO: render index
-});
+// // ROUTES ==================================
+// app.get("/", (req, res)=>{
+//     // TODO: render index
+// });
 
 // API =====================================
 app.post("/api/exercises", (req, res)=>{
     // TODO: implement create
+    db.CardioExercise.create(req.body)
+        .then(dbExercise => {
+            res.json(dbExercise);
+        })
+        .catch(err=>{
+            res.json(err);
+        })
 });
 
 app.get("/api/exercises/:id", (req, res)=>{
     // TODO: implement read
+    db.CardioExercise.findById(id)
+        .then(dbExercise=>{
+            res.json(dbExercise);
+        }).catch(err=>{
+            res.json(err);
+        })
 });
 
 app.post("/api/exercises/:id", (req, res)=>{
     // TODO: implement update
+    db.CardioExercise.findByIdAndUpdate(id, req.body)
+        .then(dbExercise=>{
+            res.json(dbExercise);
+        }).catch(err=>{
+            res.json(err);
+        })
 });
 
 app.delete("/api/exercises/:id", (req, res)=>{
     // TODO: implement delete
+    db.CardioExercise.findByIdAndDelete(id)
+        .then(dbExercise=>{
+            res.json(dbExercise);
+        }).catch(err=>{
+            res.json(err);
+        })
 });
+
+// Catch all other routes
+app.get("*", (req, res)=>{
+    res.status(404).send("Sorry, we couldn't find what you're looking for!");
+})
 
 // OPEN SERVER ================================
 app.listen(PORT, ()=>{
