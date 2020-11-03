e8073a4a8618cefcc095c8857acb6947fb4c1310
Jacob Cowan <mrjcowman@gmail.com>
2020-11-03 14:36:50 -0800

    Build create form for exercises front end

--- public/index.js
+++ public/index.js
@@ -1,3 +1,29 @@
 $(document).ready(function(){
     console.log("Loaded!");
+
+    $(".exerciseEdit form").on("submit", (event)=>{
+        event.preventDefault();
+        switch($("#selectExerciseType").val()){
+            case "cardio":
+                $.post("/api/exercises/cardio", cardioObject, (err, data)=>{
+                    if(err) throw err;
+                    console.log(data);
+                })
+        }
+    })
+
+    $("#selectExerciseType").on("change", (event)=>{
+        switch($(event.target).val()){
+            case "cardio":
+                $(".strengthInputs").hide();
+                $(".cardioInputs").show();
+                break;
+            case "strength":
+                $(".cardioInputs").hide();
+                $(".strengthInputs").show();
+                break;
+        }
+    })
+
+    $(".strengthInputs").hide();
 })
